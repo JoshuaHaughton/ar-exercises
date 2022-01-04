37c550bc0815255a1e3c6b252e47fefd2d909150
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-04 01:23:37 +0000

    Question 2 from README.md complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,22 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
+
+@store1 = Store.find(1)
+puts @store1.name
+
+@store2 = Store.find(2)
+puts @store2.name
+
+# Changes store name to "The Jalapeno Shop", 
+# then saves it to the database
+@store1.name = "The Jalapeno Shop"
+@store1.save
+
+puts @store1.name
