344caaf70407167fd94037717c1ea71526f8e26b
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-04 01:53:18 +0000

    Question 4 from README.md complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,32 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where("mens_apparel = true")
+
+# Loops through each store in the database and 
+# outputs the name and annual revenue
+i = 0
+while i < @mens_stores.length
+   store = Store.find(@mens_stores[i].id)
+    puts "#{store.name} has an annual revenue of: $#{store.annual_revenue}."
+    i += 1
+end
+
+@unprofitable_womens_apparel = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+puts "-----"
+puts @unprofitable_womens_apparel.length
+
+j = 0
+while j < @unprofitable_womens_apparel.length
+   store = Store.find(@unprofitable_womens_apparel[j].id)
+    puts "#{store.name} has an annual revenue of: $#{store.annual_revenue}."
+    j += 1
+end
